456f0efbf6eb55a2c53ea234562470832cdaa3f8
tmking2002 <tmking4@ncsu.edu>
2023-04-17 00:34:57 -0400

    Update Biggest Upsets.R

--- Biggest Upsets.R
+++ Biggest Upsets.R
@@ -3,30 +3,30 @@ library(tidyverse)
 library(gt)
 library(gtExtras)
 
-setwd("~/Desktop/Projects/softball-projects")
+setwd("~/Projects/softball-projects")
 
-source("~/Desktop/Projects/softball-projects/get_power_ratings.R")
+source("~/Projects/softball-projects/get_power_ratings.R")
 
 scoreboard_2022 <- load_espn_scoreboard(2022)
-scoreboard_2023 <- readRDS("~/Desktop/softballR-data/data/ncaa_scoreboard_2023.RDS")
+scoreboard_2023 <- load_ncaa_scoreboard(2023)
 
 standings_2023 <- get_power_ratings(scoreboard_2023) %>% 
   select(team, power_rating)
 
-logos <- scoreboard_2022 %>%
-  distinct(home_team_display_name, home_team_logo)
+logos <- scoreboard_2023 %>% 
+  distinct(home_team, home_team_logo)
 
-load("~/Desktop/Projects/softball-projects/power_rating_model.RDA")
+load("~/Projects/softball-projects/power_rating_model.RDA")
 
-start_date <- as.Date("2023-03-27")
-end_date <- as.Date("2023-04-02")
+start_date <- as.Date("2023-04-10")
+end_date <- as.Date("2023-04-16")
 
 scoreboard_test <- scoreboard_2023 %>% 
-  mutate(date = as.Date(paste0(word(date,3,sep="/"),"-",word(date,1,sep="/"),"-",word(date,2,sep="/")))) %>%
+  mutate(date = as.Date(paste0(word(game_date,3,sep="/"),"-",word(game_date,1,sep="/"),"-",word(game_date,2,sep="/")))) %>%
   filter(date <= end_date & date >= start_date) %>%
-  mutate(team1_win = team1_runs > team2_runs) %>%
-  left_join(standings_2023 %>% rename("team1_rating" = "power_rating"), by = c("team1" = "team")) %>%
-  left_join(standings_2023 %>% rename("team2_rating" = "power_rating"), by = c("team2" = "team")) %>%
+  mutate(team1_win = home_team_runs > away_team_runs) %>%
+  left_join(standings_2023 %>% rename("team1_rating" = "power_rating"), by = c("home_team" = "team")) %>%
+  left_join(standings_2023 %>% rename("team2_rating" = "power_rating"), by = c("away_team" = "team")) %>%
   drop_na(team1_rating, team2_rating) %>% 
   filter(team1_rating > 0 & team2_rating > 0) %>% 
   mutate(log_team1_rating = log(team1_rating),
@@ -39,48 +39,42 @@ scoreboard_test$likelihood <- predict(model, scoreboard_test, type = "response")
 scoreboard_test$resid <- scoreboard_test$team1_win - scoreboard_test$likelihood
 
 scoreboard_test <- scoreboard_test %>%
-  mutate(team1_runs = as.numeric(team1_runs),
-         team2_runs = as.numeric(team2_runs),
-         score = paste(team1_runs, team2_runs, sep = "-"),
+  mutate(score = paste(home_team_runs, away_team_runs, sep = "-"),
          rating_diff = abs(rating_diff),
-         log_rating_diff = abs(log_rating_diff)) %>%
-  merge(logos, by.x = "team1", by.y = "home_team_display_name") %>%
-  rename(team1_logo = home_team_logo) %>%
-  merge(logos, by.x = "team2", by.y = "home_team_display_name") %>%
-  rename(team2_logo = home_team_logo)
+         home_team = str_replace(home_team, "&amp;", "&"))
+
 
 table <- scoreboard_test %>%
   arrange(desc(abs(resid))) %>%
   head(n = 5) %>%
-  select(date,team1_logo, team1, team2_logo, team2, team1_runs, team2_runs, score, rating_diff, log_rating_diff) %>%
+  select(date,home_team_logo, home_team, away_team_logo, away_team, home_team_runs, away_team_runs, score, rating_diff) %>%
   gt() %>%
   fmt_date(date, date_style = "Md") %>%
-  cols_hide(c(team1_runs,team2_runs)) %>%
-  fmt_number(c(rating_diff, log_rating_diff), decimals = 2) %>%
-  gt_img_rows(team1_logo) %>%
-  gt_img_rows(team2_logo) %>%
+  cols_hide(c(home_team_runs,away_team_runs)) %>%
+  fmt_number(rating_diff, decimals = 2) %>%
+  gt_img_rows(home_team_logo) %>%
+  gt_img_rows(away_team_logo) %>%
   cols_label(date = "Date",
-             team1_logo = "",
-             team1 = "",
-             team2_logo = "",
-             team2 = "",
+             home_team_logo = "",
+             home_team = "",
+             away_team_logo = "",
+             away_team = "",
              score = "Score",
-             rating_diff = "Rating Diff.",
-             log_rating_diff = "Log. Rating Diff.") %>%
+             rating_diff = "Rating Diff.") %>%
   tab_style(cell_borders(sides = "right",
                          style = "dashed",
                          color = "grey"),
-            cells_body(columns = c(date, team1, team2),
+            cells_body(columns = c(date, home_team, away_team),
                        rows = everything())) %>%
   tab_style(style = cell_fill(color = "#CDEBC5"),
             locations = cells_body(
-              columns = c(team1, team1_logo),
-              rows = team1_runs > team2_runs
+              columns = c(home_team, home_team_logo),
+              rows = home_team_runs > away_team_runs
             )) %>%
   tab_style(style = cell_fill(color = "#CDEBC5"),
             locations = cells_body(
-              columns = c(team2, team2_logo),
-              rows = team1_runs < team2_runs
+              columns = c(away_team, away_team_logo),
+              rows = home_team_runs < away_team_runs
             )) %>%
   gt_theme_espn() %>%
   tab_header(title = "Biggest NCAA Softball Upsets",
